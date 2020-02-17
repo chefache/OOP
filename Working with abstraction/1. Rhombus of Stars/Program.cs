356080d53968678b6ac643db8cf61a29290408f5
Stefan Koev <stefan.m.koev@gmail.com>
2020-02-17 21:09:37 +0200

    Add files via upload

---
+++ Working with abstraction/1. Rhombus of Stars/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _1._Rhombus_of_Stars
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var rhombusDrawer = new RhombusDrawer();
+            var drawRhombus = rhombusDrawer.Draw(n);
+
+            Console.WriteLine(drawRhombus);
+        }
+    }
+}
