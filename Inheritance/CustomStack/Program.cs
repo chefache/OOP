6cd6baf7e420526f9c9a06d49a7222ddb776c3b6
Stefan Koev <stefan.m.koev@gmail.com>
2020-02-22 21:57:25 +0200

    Add files via upload

---
+++ Inheritance/CustomStack/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var stack = new StackOfStrings();
+            Console.WriteLine(stack.IsEmpty());
+            stack.AddRange(new[] { "1", "2", "3", "4" });
+            Console.WriteLine(stack.IsEmpty());
+        }
+    }
+}
