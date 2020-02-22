6f423e4532c33c5ee4613ed1e55cd641a450667c
Stefan Koev <stefan.m.koev@gmail.com>
2020-02-22 21:57:54 +0200

    Add files via upload

---
+++ Inheritance/CustomRandomList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var randomList = new RandomList();
+            randomList.Add("1");
+            randomList.Add("2");
+            randomList.Add("3");
+            randomList.Add("4");
+            randomList.Add("5");
+            randomList.Add("6");
+
+            Console.WriteLine(randomList.RemoveRandom());
+        }
+    }
+}
---
+++ Inheritance/CustomRandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rnd = new Random();
+        public string RandomElement()
+        {
+            var elementIndex = rnd.Next(1, this.Count);
+            return this[elementIndex];
+           
+        }
+        public string RemoveRandom()
+        {
+            var elementIndex = rnd.Next(1, this.Count);
+            var elemrnt = this[elementIndex];
+            this.RemoveAt(elementIndex);
+            return elemrnt;
+        }
+    }
+}
