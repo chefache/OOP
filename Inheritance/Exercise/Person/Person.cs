6fe5988b31190a353b94fd441549dc2d81bd0fba
Stefan Koev <stefan.m.koev@gmail.com>
2020-02-23 18:46:35 +0200

    Add files via upload

---
+++ Inheritance/Exercise/Person/Person.cs
@@ -0,0 +1,46 @@
+﻿namespace Person
+{
+    using System;
+    using System.Text;
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public virtual int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age cannot be less than 0");
+                }
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(string.Format($"Name: {this.Name}, Age: {this.Age}"));
+            return sb.ToString();
+        }
+    }
+}
