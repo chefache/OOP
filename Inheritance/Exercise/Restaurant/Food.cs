e402e9af5664ee79ff0d0ea3dd874e9affa23e82
Stefan Koev <stefan.m.koev@gmail.com>
2020-02-24 20:08:38 +0200

    Add files via upload

---
+++ Inheritance/Exercise/Restaurant/Food.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Food : Products
+    {
+        public Food(string name, decimal price, double grams) 
+            : base(name, price)
+        {
+            this.Grams = grams;
+        }
+        public double Grams { get; set; }
+    }
+}
---
+++ Inheritance/Exercise/Restaurant/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Products
+    {
+        public Products(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
