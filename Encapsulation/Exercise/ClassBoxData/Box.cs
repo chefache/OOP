8172b52106f5d7f0120707fb67f06ac5e9849eb3
Stefan Koev <stefan.m.koev@gmail.com>
2020-02-28 23:42:20 +0200

    Add files via upload

---
+++ Encapsulation/Exercise/ClassBoxData/Box.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+	public class Box
+	{
+		private double length;
+		private double width;
+		private double height;
+
+		public Box(double length, double width, double height)
+		{
+			this.Length = length;
+			this.Width = width;
+			this.Height = height;
+		}
+
+		public double Length
+		{
+			get => length;
+
+			private set
+			{
+				if (value <= 0)
+				{
+					throw new Exception("Length cannot be zero or negative.");
+				}
+				this.length = value;
+			}
+		}
+
+		public double Width
+		{
+			get => width;
+
+			private set
+			{
+				if (value <= 0)
+				{
+					throw new Exception("Width cannot be zero or negative.");
+				}
+				width = value;
+			}
+		}
+
+		public double Height
+		{
+			get => height; 
+
+			private set
+			{
+				if (value <= 0)
+				{
+					throw new Exception("Height cannot be zero or negative.");
+				}
+				height = value;
+			}
+		}
+		public double SurfaceArea(double length, double width, double height)
+		{
+			return 2 * (this.length * this.width) + 
+				2 * (this.length * this.height) + 
+				2 * (this.width * this.height);
+		}
+		public double LateralSurface(double length, double width, double height)
+		{
+			return 2 * (this.length * this.height) + 
+				2 * (this.width * this.height);
+		}
+		public double Volume(double length, double width, double height)
+		{
+			return this.length * this.width * this.height;
+		}
+
+	}
+}
---
+++ Encapsulation/Exercise/ClassBoxData/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            var box = new Box(length, width, height);
+
+            Console.WriteLine($"Surface Area - {box.SurfaceArea(length, width, height):f2}");
+            Console.WriteLine($"Lateral Surface - {box.LateralSurface(length, width, height):f2}");
+            Console.WriteLine($"Volume - {box.Volume(length, width, height):f2}");
+        }
+    }
+}
