62de13d7d562982a16b4f10fcfb6f22bc0fc0098
Stefan Koev <stefan.m.koev@gmail.com>
2020-03-01 22:48:03 +0200

    Add files via upload

---
+++ Encapsulation/Exercise/PizzaCalories/DoughValidator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public static class DoughValidator
+    {
+       private static Dictionary<string, double> flourTypes;
+       private static Dictionary<string, double> bakingTechniques;
+
+        public static bool IsValidFlourType(string type)
+        {
+            if (flourTypes == null || bakingTechniques == null)
+            {
+                Initialize();
+            }
+
+            return flourTypes.ContainsKey(type);
+        }
+
+        public static bool IsValidBakingTechnique(string type)
+        {
+            if (flourTypes == null || bakingTechniques == null)
+            {
+                Initialize();
+            }
+
+            return bakingTechniques.ContainsKey(type);
+        }
+
+        public static double GetFlourModifier(string type)
+       => flourTypes[type];
+        public static double GetBakingTechniqueModifier(string type)
+        => bakingTechniques[type];
+
+        private static void Initialize()
+        {
+            flourTypes = new Dictionary<string, double>();
+            bakingTechniques = new Dictionary<string, double>();
+
+            flourTypes.Add("White", 1.5);
+            flourTypes.Add("WholeGrain", 1);
+
+            bakingTechniques.Add("Crispy", 0.9);
+            bakingTechniques.Add("Chewy", 1.1);
+            bakingTechniques.Add("Homemade", 1);
+        }
+    }
+}
---
+++ Encapsulation/Exercise/PizzaCalories/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace PizzaCalories
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            string flourType = input[1];
+            string bakingTechnique = input[2];
+            double weight = double.Parse(input[3]);
+
+            var dough = new Dough(weight, flourType, bakingTechnique);
+
+            Console.WriteLine($"{dough.GetTotalCalories():f2}");
+        }
+    }
+}
