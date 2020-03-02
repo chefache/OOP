b32fc93dee692e75d9f8097e7092ca8a05ece4ee
Stefan Koev <stefan.m.koev@gmail.com>
2020-03-02 17:22:15 +0200

    Add files via upload

---
+++ Encapsulation/Exercise/FootballTeamGenerator/Player.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballTeamGenerator
+{
+    public class Player
+    {
+		private string name;
+		private int endurance;
+		private int sprint;
+		private int passing;
+		private int shooting;
+		private int dribble;
+
+		public Player(string name, int endurance, int sprint, int passing, int shooting, int dribble)
+		{
+			this.Name = name;
+			this.Endurance = endurance;
+			this.Sprint = sprint;
+			this.Passing = passing;
+			this.Shooting = shooting;
+			this.Dribble = dribble;
+		}
+
+		public string Name
+		{
+			get { return name; }
+
+			private set 
+			{
+				if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+				{
+					throw new Exception("A name should not be empty.");
+				}
+				this.name = value; 
+			}
+		}
+
+		public int Endurance
+		{
+			get { return endurance; }
+
+			private set
+			{
+				StatValidator(value);
+				this.endurance = value;
+			}
+		}
+	
+
+		public int Sprint
+		{
+			get { return sprint; }
+
+			private set 
+			{
+				StatValidator(value);
+				this.sprint = value;
+			}
+		}
+
+	     public int Passing
+		{
+			get { return passing; }
+
+			private set 
+			{
+				StatValidator(value);
+				this.passing = value; 
+			}
+		}
+
+		public int Shooting
+		{
+			get { return shooting; }
+
+			private set 
+			{
+				StatValidator(value);
+				this.shooting = value; 
+			}
+		}
+
+		public int Dribble
+		{
+			get { return dribble; }
+
+			private set 
+			{
+				StatValidator(value);
+				this.dribble = value; 
+			}
+		}
+
+
+		private static void StatValidator(int value)
+		{
+			if (value < 0 || value > 100)
+			{
+				throw new ArgumentException($"{value} should be between 0 and 100.");
+			}
+		}
+
+		public int overallSkillLevel()
+		{
+			return Math.Abs((this.endurance + this.sprint + this.passing + this.shooting + this.dribble) / 5);
+		}		
+	}	
+}
---
+++ Encapsulation/Exercise/FootballTeamGenerator/Team.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FootballTeamGenerator
+{
+	public class Team
+	{
+		private List<Player> players;
+		private string name;
+
+		public Team(string name)
+		{
+			this.Name = name;
+			this.players = new List<Player>();
+		}
+
+		public string Name
+		{
+			get
+			{
+				return name;
+			}
+			private set
+			{
+				if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+				{
+					throw new Exception("A name should not be empty.");
+				}
+				this.name = value;
+			}
+		}
+
+		public List<Player> Players
+		{
+			get
+			{
+				return Players;
+			}
+			private set
+			{
+				this.Players = value;
+			}
+		}
+
+		public int Rating
+		{
+			get
+			{
+				return CalculateTeamRating();
+			}
+		}
+
+		private int CalculateTeamRating()
+		{
+			if (this.players.Any())
+			{
+				return (int)Math.Round(this.players.Average(p => p.overallSkillLevel()));
+			}
+			else
+			{
+				return 0;
+			}
+		}
+
+		public void AddPlayer(Player player)
+		{
+			this.players.Add(player);
+		}
+
+		public void RemovePlayer(string player)
+		{
+			if (!this.players.Any(p => p.Name == player))
+			{
+				throw new ArgumentException($"Player {player} is not in {this.Name} team. ");
+			}
+
+			Player pl = this.players.FirstOrDefault(p => p.Name == player);
+			this.players.Remove(pl);
+		}
+
+		public override string ToString()
+		{
+			return $"{this.name} - {this.Rating}";
+		}
+	}
+}
