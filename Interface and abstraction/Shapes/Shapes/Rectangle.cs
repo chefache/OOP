91d3cc7d2017e6e7ccbc050940dd3bc0ea7fe015
Stefan Koev <stefan.m.koev@gmail.com>
2020-03-03 17:21:21 +0200

    Add files via upload

---
+++ Interface and abstraction/Shapes/Shapes/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes.Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public int Width { get; }
+        public int Height { get; }
+
+        public void Draw()
+        {
+            Console.WriteLine(new string('*', this.Width));
+
+            for (int i = 0; i < this.Height - 2; i++)
+            {
+                Console.WriteLine('*' + new string(' ', this.Width - 2) + '*');
+            }
+
+            Console.WriteLine(new string('*', this.Width));
+        }
+    }
+}
---
+++ Interface and abstraction/Shapes/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using Shapes.Shapes;
+using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            IDrawable drawRectangle = new Rectangle(10, 4);
+            drawRectangle.Draw();
+
+            IDrawable drawCircle = new Circle(20);
+            drawCircle.Draw();
+        }
+    }
+}
