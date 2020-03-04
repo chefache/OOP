e386f6881951c8013af546771f3b12f86e6ee41f
Stefan Koev <stefan.m.koev@gmail.com>
2020-03-04 20:47:38 +0200

    Add files via upload

---
+++ Encapsulation/Exercise/FootballTeamGenerator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FootballTeamGenerator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+			try
+			{
+                var teams = new List<Team>();
+
+                string command = Console.ReadLine();
+
+                while (command != "END")
+                {
+                    var commandArgs = command
+                        .Split(";", StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+
+                    string addOrRemoveCommand = commandArgs[0];
+
+                    string teamName = commandArgs[1];
+
+                    if (addOrRemoveCommand == "Team")
+                    {
+                        teams.Add(new Team(teamName));
+                    }
+
+                    var team = teams.FirstOrDefault(p => p.Name == teamName);
+
+                    if (team == null)
+                    {
+                        throw new Exception($"Team {teamName} does not exist.");
+                    }
+
+                    switch (addOrRemoveCommand)
+                    {
+                        case "Add":
+                            team.AddPlayer(new Player(commandArgs[2],
+                                int.Parse(commandArgs[3]),
+                                int.Parse(commandArgs[4]),
+                                int.Parse(commandArgs[5]),
+                                int.Parse(commandArgs[6]),
+                                int.Parse(commandArgs[7])));
+                            break;
+                        case "Remove":
+                            team.RemovePlayer(commandArgs[2]);
+                            break;
+                        case "Rating":
+                            Console.WriteLine($"{team.Name} - {team.GetRating()}");
+                            break;
+                        default:
+                            break;
+                    }
+                    command = Console.ReadLine();
+                }
+            }
+			catch (Exception ex)
+			{
+
+                Console.WriteLine(ex);
+			}
+        }
+    }
+}
---
+++ Encapsulation/Exercise/FootballTeamGenerator/Team.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FootballTeamGenerator
+{
+	public class Team
+	{
+
+		private string name;
+		private readonly List<Player> players;
+
+		public Team(string name)
+		{
+			this.Name = name;
+			this.players = new List<Player>();
+		}
+
+		public string Name
+		{
+			get 
+			{ 
+				return name; 
+			}
+			private set 
+			{
+				if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+				{
+					throw new Exception("A name should not be empty.");
+				}
+				this.name = value; 
+			}
+		}
+
+		public void AddPlayer(Player player)
+		{
+			players.Add(player);
+		}
+
+		public void RemovePlayer(string playerName)
+		{
+			var playerToRemove = this.players.FirstOrDefault(p => p.Name == playerName);
+			if (playerToRemove == null)
+			{
+				throw new Exception($"Player {playerName} is not in {this.name} team.");
+			}
+			this.players.Remove(playerToRemove);
+		}
+
+		public int GetRating()
+		{
+			if (this.players.Count == 0)
+			{
+				return 0;
+			}
+
+			double sumAvSkillLevels = this.players.Sum(p => p.playerOverallSkill());
+			return (int)Math.Round(sumAvSkillLevels / this.players.Count);
+		}
+
+	}
+}
