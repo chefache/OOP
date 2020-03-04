0c0a4c951b0f13a992a31365802df32ff939cb5d
Stefan Koev <stefan.m.koev@gmail.com>
2020-03-04 20:45:42 +0200

    Add files via upload

---
+++ Interface and abstraction/Cars/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cars
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ICar seat = new Seat("leon", "Grey");
+            ICar tesla = new Tesla("Model 3", "Red", 2);
+
+            Console.WriteLine(seat);
+            seat.Start();
+            seat.Stop();
+            Console.WriteLine(tesla);
+            tesla.Start();
+            tesla.Stop();
+        }
+    }
+}
