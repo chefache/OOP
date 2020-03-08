bc5585bfe09e64d43e5f2a050fe3b6bb08c5f7e3
Stefan Koev <stefan.m.koev@gmail.com>
2020-03-08 23:00:04 +0200

    Add files via upload

---
+++ Interface and abstraction/Exercise/PersonInfo/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthDate = Console.ReadLine();
+
+            IIdentifiable identifiable = new Citizen(name, age, id, birthDate);
+            IBirthable birthable = new Citizen(name, age, id, birthDate);
+            Console.WriteLine(identifiable.Id);
+            Console.WriteLine(birthable.BirthDate);
+        }
+    }
+}
