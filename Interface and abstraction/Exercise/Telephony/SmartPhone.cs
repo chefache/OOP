a756ddddee73ecd09e7e5de1f998e3d218696339
Stefan Koev <stefan.m.koev@gmail.com>
2020-03-09 20:13:08 +0200

    Add files via upload

---
+++ Interface and abstraction/Exercise/Telephony/SmartPhone.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Telephony
+{
+    public class SmartPhone : ICallable, IBrowsable
+    {
+        private string phoneNumber;
+        private string url;
+
+        public SmartPhone(string phoneNumber, string url)
+        {
+            this.PhoneNumber = phoneNumber;
+            this.Url = url;
+        }
+
+        public string PhoneNumber
+        {
+            get => phoneNumber;
+            set
+            {
+                PhoneValidator(value);
+                phoneNumber = value;
+            }
+        }
+        public string Url
+        {
+            get => url;
+            set
+            {
+                UrlValidator(value);
+                url = value;
+            }
+        }
+
+        public string Call()
+        {
+            return $"Calling...{PhoneNumber}";
+        }
+
+        public string Browse()
+        {
+            return $"browsing: {Url}";
+        }
+
+        public void PhoneValidator(string value)
+        {
+            if (Regex.IsMatch(value, ".*?[a-zA-Z].*?"))
+            {
+                throw new Exception("Invalid number!");
+            }
+        }
+
+        public void UrlValidator(string value)
+        {
+            if (Regex.IsMatch(value, ".*?[0-9].*?"))
+            {
+                throw new Exception("Invalid URL!");
+            }
+        }
+    }
+}
---
+++ Interface and abstraction/Exercise/Telephony/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telephony
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+			try
+			{
+                var numbers = Console.ReadLine()
+                .Split();
+
+                var urls = Console.ReadLine()
+                    .Split();
+
+                var phonesSet = new HashSet<string>();
+                var urlsSet = new HashSet<string>();
+
+                foreach (var number in numbers)
+                {
+                    foreach (var url in urls)
+                    {
+                        var phone = new SmartPhone(number, url);
+
+                        phonesSet.Add(phone.Call());
+                        urlsSet.Add(phone.Browse());
+                    }
+                }
+
+                Console.WriteLine(string.Join("\n", phonesSet));
+                Console.WriteLine(string.Join("\n", urlsSet));
+            }
+			catch (Exception ex)
+			{
+
+                Console.WriteLine(ex.Message);
+			}
+        }
+    }
+}
