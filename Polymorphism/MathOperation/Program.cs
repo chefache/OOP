af4fcd93dd1c575fcfab1fd4ad1279ead2c69622
Stefan Koev <stefan.m.koev@gmail.com>
2020-03-11 19:21:04 +0200

    Add files via upload

---
+++ Polymorphism/MathOperation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MathOperation
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            MathOperations mo = new MathOperations();
+
+            Console.WriteLine(mo.Add(1,2));
+            Console.WriteLine(mo.Add(1.1,2.1,3.1));
+            Console.WriteLine(mo.Add(1.3m, 2.6m, 5.5m));
+        }
+    }
+}
