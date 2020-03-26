daca1eb7c87de8b474b4c3bd157a9d2b399fc94f
Stefan Koev <stefan.m.koev@gmail.com>
2020-03-26 16:39:16 +0200

    Add files via upload

---
+++ Reflection/Stealer/Spy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string nameOfClass, params string[] fieldToInvestigate)
+        {
+            Type typeOfClass = Type.GetType(nameOfClass);
+
+            FieldInfo[] classFields = typeOfClass.GetFields(               
+                 BindingFlags.Instance
+                | BindingFlags.Static
+                | BindingFlags.NonPublic
+                | BindingFlags.Public);
+
+            var sb = new StringBuilder();
+
+            object classInstance = Activator.CreateInstance(typeOfClass, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {nameOfClass}");
+
+            foreach (var field in classFields.Where(f => fieldToInvestigate.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
