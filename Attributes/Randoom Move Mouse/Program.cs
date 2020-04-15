4422dec5493dfe4da3bda17df0c683c6b51cdbcf
Stefan Koev <stefan.m.koev@gmail.com>
2020-04-15 11:51:15 +0300

    Add files via upload

---
+++ Attributes/Randoom Move Mouse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Randoom_Move_Mouse
+{
+    public class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int SetCursorPos(int x, int y);
+
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+
+            for (int i = 0; i < 500; i++)
+            {
+                Console.WriteLine("Where is the mouse .... :D ?");
+                SetCursorPos(rnd.Next(0, 2000), rnd.Next(0, 2000));
+                Thread.Sleep(300);
+                Console.Beep(300, 100);
+            }
+        }
+    }
+}
