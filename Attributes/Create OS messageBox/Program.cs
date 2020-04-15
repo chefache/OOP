cc037aaebb1a108d0090fe24c3edaae9ca778d92
Stefan Koev <stefan.m.koev@gmail.com>
2020-04-15 11:50:56 +0300

    Add files via upload

---
+++ Attributes/Create OS messageBox/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Create_OS_messageBox
+{
+    class Program
+    {
+        [DllImport("user32.dll", EntryPoint = "MessageBox")]
+
+        public static extern int ShowMessageBox(int hWnd, string text, string caption, int type);
+
+        static void Main(string[] args)
+        {
+            ShowMessageBox(0, "Hello Stefan", "My project", 1); // Possibla change of type 1, 2, 3...
+        }
+    }
+}
